e3177e45f4a4e7640159375219b5550a6133c6af
Bastian Hofmann <bashofmann@googlemail.com>
2011-09-24 13:14:33 +0200

    added proxied oauth request to statusnet

--- proxiedContent.php
+++ proxiedContent.php
@@ -46,3 +46,49 @@ $data = json_decode(file_get_contents('php://input'), true);
     <?php endforeach; ?>
 </ul>
 <?php endif; ?>
+
+
+<script type="text/os-template" xmlns:os="http://ns.opensocial.org/2008/markup" require="feed" autoUpdate="true">
+    <h3>Your status.net feed:</h3>
+    <ul>
+        <li repeat="${feed}">
+            ${Cur.text} (${Cur.created_at}, ${Cur.user.name})
+        </li>
+    </ul>
+</script>
+
+<script type="text/javascript">
+    function loadFeed() {
+        var fetchData = function() {
+            var params = {};
+            params[gadgets.io.RequestParameters.AUTHORIZATION] = gadgets.io.AuthorizationType.OAUTH;
+            params[gadgets.io.RequestParameters.CONTENT_TYPE] = gadgets.io.ContentType.JSON;
+            params[gadgets.io.RequestParameters.OAUTH_SERVICE_NAME]='statusnet';
+            gadgets.io.makeRequest('http://dev.status.net:8080/index.php/api/statuses/home_timeline.json', function(response) {
+
+                if (response.oauthApprovalUrl) {
+                    var popup = new gadgets.oauth.Popup(
+                        response.oauthApprovalUrl,
+                        'width=400&height=400',
+                        function() { },
+                        function() {
+                            fetchData();
+                        }
+                        );
+
+                    popup.onClick_();
+                } else {
+                    console.log(response);
+                    opensocial.data.DataContext.putDataSet('feed', response.data);
+                    gadgets.window.adjustHeight();
+                }
+            }, params);
+        }
+
+        fetchData();
+    }
+    
+    gadgets.util.registerOnLoadHandler(function() {
+        loadFeed();
+    });
+</script>
